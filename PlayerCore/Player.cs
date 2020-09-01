6d612fb9ec77ac07514746644a8c5c93e3258080
Nawiarra <nika.berezovskaya@ukr.net>
2020-09-01 13:59:59 +0300

    Add class player, that use one of base types players

---
+++ PlayerCore/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerCore
+{
+    class Player
+    {
+        public string Name { get; private set; }
+        public BasePlayer PlayerType { get; private set; }
+
+        Player(string name, BasePlayer type)
+        {
+            Name = name;
+            PlayerType = type;
+        }
+
+    }
+}
