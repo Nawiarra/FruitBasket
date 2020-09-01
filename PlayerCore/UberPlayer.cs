4f75d60213fce33744a1793f82a53bbbd670572c
Nawiarra <nika.berezovskaya@ukr.net>
2020-09-01 13:05:06 +0300

    Add uber-player logic

---
+++ PlayerCore/UberPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerCore
+{
+    class UberPlayer : BasePlayer
+    {
+        private int CurrentValueForSelect = 40;
+        public override void SelectNextNumber()
+        {
+            if (index < size)
+            {
+                arrayNumbers[index] = CurrentValueForSelect + index;
+                
+                index++;
+            }
+        }
+
+        public override string Victory()
+        {
+            return "Well, it was easy!";
+        }
+    }
+}
