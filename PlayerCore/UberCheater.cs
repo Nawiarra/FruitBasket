fb1e4905ad2e7d5d20b2cf5be404a8bbc98267ee
Nawiarra <nika.berezovskaya@ukr.net>
2020-09-01 13:25:33 +0300

    Add uber-cheater logic

---
+++ PlayerCore/UberCheater.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerCore
+{
+    class UberCheater : BasePlayer
+    {
+        private int CurrentValueForSelect = 40;
+        public override void SelectNextNumber()
+        {
+            bool isUnique = false;
+
+            if (index < size)
+            {
+                while (!isUnique)
+                {
+                    foreach (int number in NumbersForCheatersOnly)
+                    {
+                        if (CurrentValueForSelect == number)
+                        {
+                            CurrentValueForSelect++;
+
+                            break;
+                        }
+                    }
+
+                    isUnique = true;
+
+                    arrayNumbers[index] = CurrentValueForSelect;
+
+                    NumbersForCheatersOnly.Add(arrayNumbers[index]);
+
+                    index++;
+                }
+            }
+        }
+
+        public override string Victory()
+        {
+            return "Evil laugh x2";
+        }
+    }
+}
