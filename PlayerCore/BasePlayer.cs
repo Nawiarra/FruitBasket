2f025610216e9c8b0fcee7f8e5b565133419d773
Nawiarra <nika.berezovskaya@ukr.net>
2020-09-01 12:26:58 +0300

    Add base player programm logic

---
+++ PlayerCore/BasePlayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerCore
+{
+    public class BasePlayer
+    {
+        private static int size = 20;
+
+        private int[] arrayNumbers;
+        public int index { get; private set; }
+        public int[] ArrayNumbers{get; private set;}
+
+        public BasePlayer()
+        {
+            arrayNumbers = new int[size];
+            index = 0;
+        }
+
+        public virtual void SelectNextNumber()
+        {
+            Random rnd = new Random();
+            arrayNumbers[index] = rnd.Next(40, 140);
+        }
+
+
+    }
+}
