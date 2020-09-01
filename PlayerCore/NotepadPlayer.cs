be7655f129dc435158a85ba7e3f74c3dc6f58a8a
Nawiarra <nika.berezovskaya@ukr.net>
2020-09-01 12:58:59 +0300

    Add palyer-notepad logic

---
+++ PlayerCore/NotepadPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerCore
+{
+    class NotepadPlayer : BasePlayer
+    {
+        public override void SelectNextNumber()
+        {
+            bool isUnique = false;
+
+            Random rnd = new Random();
+
+            if (index < size)
+            {
+                while (!isUnique)
+                {
+                    arrayNumbers[index] = rnd.Next(40, 140);
+
+                    for (int i = 0; i < index; i++)
+                    {
+                        if(arrayNumbers[index] == arrayNumbers[i])
+                        {
+                            break;
+                        }
+                    }
+
+                    isUnique = true;
+
+                    index++;
+                }
+            }
+        }
+        public override string Victory()
+        {
+            return "Okay, I am writing this in my notepad!";
+        }
+    }
+}
