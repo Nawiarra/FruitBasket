ea880098cf3ba30675457cd17e31aa305813560d
Nawiarra <nika.berezovskaya@ukr.net>
2020-09-01 14:58:36 +0300

    Add validation library

---
+++ ValidationLib/Validation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationLib
+{
+    public class Validation
+    {
+        public static int IntValidation(string line)
+        {
+            int result;
+
+            if (!int.TryParse(line, out result))
+                return int.MinValue;
+            else
+                return result;
+        }
+    }
+}
